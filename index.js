a642617c3f00d5dbd76d5a87710efb0c46a6bc17
Premananth K <premananth_k@live.com>
2018-06-18 16:14:47 +0530

    Send SMS

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
-
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
+  apiKey: "bb3411a9",
+  apiSecret: "q8sjJ8B3svMDSEOn"
+});
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -8,3 +12,14 @@ app.get('/', function (req, res) {
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!.')
 })
+nexmo.message.sendSms(
+  "919791532202", '919791532202', 'this is test',
+    (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.dir(responseData);
+      }
+    }
+ );
+  
